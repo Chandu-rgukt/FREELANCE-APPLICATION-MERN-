885fb2de6c084633d7931c038e72a4d2052f9d99
chandu <chandrasekharjnvnellore@gmail.com>
2025-06-27 12:57:24 +0530

    commit messge

---
+++ server/SocketHandler.js
@@ -0,0 +1,121 @@
+
+import {Chat, Project} from "./Schema.js";
+
+import {v4 as uuid} from 'uuid';
+
+
+const SocketHandler=(socket) =>{
+
+socket.on("join-chat-room", async({projectid, freelancerId})=>{
+
+const project= await Project.findById(projectId);
+
+if (project.freelancerId== freelancerId) {
+
+await socket.join(projectId);
+
+console.log(socket.rooms);
+
+socket.broadcast.to(projectId).emit("user-joined-room");
+
+const chats= await Chat. FindById(projectId);
+
+if(!chats) {
+    const newchat=new chat({
+        _id:projectId,
+        message:[]
+    })
+await newchat.save();
+}
+await socket.emit('message-updated',{chats});
+}
+})
+
+socket.on("join-chat-room-client",async({projectId})=>{
+    const project=await project.findById(projectId);
+    if(project.status=="Assigned"||project.status=="complets")
+    {
+        await socket.join(projectId);
+        console.log(socket.rooms);
+        socket.broadcast.to(projectId).emit("user-joined-room");
+
+const chats= await Chat.findById(projectId);
+
+if(!chats) {
+
+const newChat = new Chat({
+
+id: projectId,
+
+messages: []
+
+})
+
+await newChat.save();
+
+}
+
+
+await socket.emit('messages-updated', (chats));
+
+}
+
+})
+
+socket.on('update-messages', async ({ projectId }) => {
+
+try {
+
+const chat =await Chat.findOne({projectId});
+
+console.log('updating messages');
+
+socket.emit('messages-updated', (chat));
+
+} catch (error) {
+
+console.error('Error updating messages:', error);
+    }
+});
+
+    
+    socket.on("new-message", async ({ projectid, senderId, message, time}) => {
+    
+    try {
+    
+    await Chat.findOneAndUpdate(
+    
+    {_id: projectId},
+    
+    { $addToSet: { messages: {id: uuid(), text: message, senderId, time}}}, 
+    {new: true}
+    
+    );
+    
+    const chat= await Chat.findOne({_id: projectId });
+    
+    console.log(chat);
+    
+    socket.emit('messages-updated', (chat));
+    
+    socket.broadcast.to (projectId).emit('message-from-user');
+    
+    } catch (error) {
+    
+    console.error('Error adding new message:', error);
+    
+    }
+    
+});
+    
+}
+    
+export default SocketHandler;
+    
+    
+
+
+
+
+
+
